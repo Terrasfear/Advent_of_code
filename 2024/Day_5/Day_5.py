5f3ec1775457e7e4bf8628a8e5a63f83a19b076e
Casper <caspervanderhorst@gmail.com>
2024-12-06 01:24:33 +0100

    Day 5 part 1, 2 wip

---
+++ 2024/Day_5/Day_5.py
@@ -0,0 +1,48 @@
+class Rules:
+    def __init__(self, rules):
+        self.rules = rules
+
+    def __call__(self, lhs, rhs):
+        if (lhs, rhs) in self.rules:
+            return True
+        elif (rhs, lhs) in self.rules:
+            return False
+        else:
+            raise Exception(f"no rule with both {lhs} and {rhs}")
+
+
+def testUpdate(update: list[int], rules: Rules):
+    for i in range(len(update)-1):
+        if not rules(update[i], update[i+1]):
+            return False
+    return True
+
+def reorderUpdate(update: list[int], rules: Rules):
+    # todo
+    update.sort()
+
+
+with open("Input", "r") as _file:
+    _lines = iter(_file.readlines())
+
+    _rule_list: list[tuple[int, int]] = []
+    _updates: list[list[int]] = []
+
+    while (line := next(_lines)) != "\n":
+        _rule_list.append(tuple(map(int, line.removesuffix("\n").split("|"))))
+
+    _rules = Rules(_rule_list)
+
+    while line := next(_lines, None):
+        _updates.append(list(map(int, line.removesuffix("\n").split(","))))
+
+_middles_sum = 0
+_reordered_middles_sum = 0
+for update in _updates:
+    if testUpdate(update, _rules):
+        _middles_sum += update[len(update) // 2]
+    else:
+        reorderUpdate(update, _rules)
+        _reordered_middles_sum += update[len(update) // 2]
+
+print(f"Part 1: {_middles_sum}")
